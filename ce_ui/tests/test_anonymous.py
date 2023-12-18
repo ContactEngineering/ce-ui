c64edd5b873f72de39bd843ea88a3e05fa9e1550
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-12-18 11:17:42 +0100

    MAINT: Moved terms and conditions related code to UI

--- ce_ui/tests/test_anonymous.py
+++ ce_ui/tests/test_anonymous.py
@@ -1,7 +1,9 @@
 import pytest
 from django.shortcuts import reverse
+from django.utils import timezone
 
 from topobank.utils import assert_in_content, assert_not_in_content
+from termsandconditions.models import TermsAndConditions
 
 
 #
@@ -19,3 +21,16 @@ def test_anonymous_user_only_published_as_default(client):
     assert_not_in_content(response, 'Only surfaces shared by you')
     assert_not_in_content(response, 'Only surfaces published by you')
     assert_in_content(response, 'Only surfaces published by others')
+
+
+@pytest.mark.django_db
+def test_terms_conditions_as_anonymous(client):
+    # Install terms and conditions for test
+    TermsAndConditions.objects.create(slug='test-terms', name="Test of T&amp;C",
+                                      text="some text", date_active=timezone.now())
+
+    response = client.get(reverse('terms'))
+    assert_in_content(response, "Test of T&amp;C")
+
+    response = client.get(reverse('tc_accept_specific_page', kwargs=dict(slug='test-terms')))
+    assert_in_content(response, "some text")
