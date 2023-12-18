415f69310ad039beea03bc2a0d32eb94f6a15d77
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-12-18 10:56:57 +0100

    MAINT: UI routes are now registerd by the UI plugin

--- ce_ui/urls.py
+++ ce_ui/urls.py
@@ -1,10 +1,78 @@
-from django.urls import re_path, path
+from django.conf import settings
+from django.urls import include, re_path, path
+from django.views.generic import TemplateView, RedirectView
 
 from . import views
 
 app_name = "ce_ui"
-urlprefix='ui/'
+
+urlprefix = None  # No url prefix, this plugin wants to register top-level routes
+
+#
+# Top-level routes
+#
 urlpatterns = [
+    #
+    # Main entry points and static pages
+    #
+    path("", views.HomeView.as_view(), name="home"),
+    path(
+        "about/",
+        TemplateView.as_view(template_name="pages/about.html"),
+        name="about",
+    ),
+    path(
+        "termsandconditions/",
+        views.TermsView.as_view(),
+        name="terms",
+    ),
+    path(
+        "help/",
+        views.HelpView.as_view(),
+        name="help",
+    ),
+    path(
+        "search/",
+        RedirectView.as_view(pattern_name='ce_ui:select'),
+        name="search",
+    ),
+    #
+    # For asking for terms and conditions
+    #
+    # some url specs are overwritten here pointing to own views in order to plug in
+    # some extra context for the tabbed interface
+    # View Specific Active Terms
+    re_path(r'^terms/view/(?P<slug>[a-zA-Z0-9_.-]+)/$', views.TermsDetailView.as_view(),
+            name="tc_view_specific_page"),
+
+    # View Specific Version of Terms
+    re_path(r'^terms/view/(?P<slug>[a-zA-Z0-9_.-]+)/(?P<version>[0-9.]+)/$', views.TermsDetailView.as_view(),
+            name="tc_view_specific_version_page"),
+
+    # Print Specific Version of Terms
+    re_path(r'^terms/print/(?P<slug>[a-zA-Z0-9_.-]+)/(?P<version>[0-9.]+)/$',
+            views.TermsDetailView.as_view(template_name="termsandconditions/tc_print_terms.html"),
+            name="tc_print_page"),
+
+    # Accept Terms
+    re_path(r'^terms/accept/$', views.TermsAcceptView.as_view(), name="tc_accept_page"),
+
+    # Accept Specific Terms
+    re_path(r'^terms/accept/(?P<slug>[a-zA-Z0-9_.-]+)$', views.TermsAcceptView.as_view(),
+            name="tc_accept_specific_page"),
+
+    # Accept Specific Terms Version
+    re_path(r'^terms/accept/(?P<slug>[a-zA-Z0-9_.-]+)/(?P<version>[0-9\.]+)/$', views.TermsAcceptView.as_view(),
+            name="tc_accept_specific_version_page"),
+
+    # the defaults
+    re_path(r'^terms/', include('termsandconditions.urls')),
+]
+
+#
+# Routes under the 'ui/' prefix
+#
+ui_urlpatterns = [
     #
     # HTML routes
     #
@@ -90,3 +158,16 @@ urlpatterns = [
         name='unselect-all'
     )
 ]
+urlpatterns += [path('ui/', include((ui_urlpatterns, app_name)))]
+
+#
+# Challenge redirect
+#
+if settings.CHALLENGE_REDIRECT_URL:
+    urlpatterns += [
+        path(
+            "challenge/",
+            RedirectView.as_view(url=settings.CHALLENGE_REDIRECT_URL, permanent=False),
+            name="challenge",
+        ),
+    ]
