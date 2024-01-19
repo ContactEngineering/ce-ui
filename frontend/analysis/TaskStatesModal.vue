1b660ff678bd5b279ff296c838b43953b5c279ff
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-01-19 14:10:42 +0100

    MAINT: Fix close button of modal windows

--- frontend/analysis/TaskStatesModal.vue
+++ frontend/analysis/TaskStatesModal.vue
@@ -58,8 +58,8 @@ export default {
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="taskStatesModalLabel">Tasks</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
+                    <button class="btn-close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"></span>
                     </button>
                 </div>
                 <div class="modal-body">
