ff4136c3a6ad05035b84167896291294ac1876e1
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-01-19 14:22:41 +0100

    ENH: Trace and report peak memory usage of tasks

--- frontend/analysis/TaskStateRow.vue
+++ frontend/analysis/TaskStateRow.vue
@@ -28,18 +28,18 @@ export default {
     methods: {
         scheduleStateCheck() {
             // Tasks are still pending or running if this state check is scheduled
-            if (this._analysis !== null) {
-                if (this._function === null) {
+            if (this._analysis != null) {
+                if (this._function == null) {
                     axios.get(this._analysis.function)
                         .then(response => {
                             this._function = response.data;
                         });
                 }
 
-                if (this._subject === null) {
+                if (this._subject == null) {
                     const subject = this._analysis.subject;
-                    const subjectUrl = subject.topography !== null ?
-                        subject.topography : subject.surface !== null ?
+                    const subjectUrl = subject.topography != null ?
+                        subject.topography : subject.surface != null ?
                             subject.surface : subject.collection;
                     axios.get(subjectUrl)
                         .then(response => {
@@ -51,7 +51,7 @@ export default {
                     setTimeout(this.checkState, this.pollingInterval);
                 } else if (this._analysis.task_state == 'fa') {
                     // This is a failure. Query reason.
-                    if (this._analysis.error === null) {
+                    if (this._analysis.error == null) {
                         // The analysis function did not raise an exception itself. This means it actually finished and
                         // we have a result.json, that should contain an error message.
                         axios.get(`${this._analysis.data_prefix}result.json`)
@@ -71,7 +71,7 @@ export default {
         checkState() {
             axios.get(this._analysis.url)
                 .then(response => {
-                    if (this._analysis === null || this._analysis.task_state !== response.data.task_state) {
+                    if (this._analysis == null || this._analysis.task_state !== response.data.task_state) {
                         // State has changed
                         this.$emit('setTaskState', response.data.task_state);
                     }
@@ -92,7 +92,7 @@ export default {
     },
     computed: {
         analysisId() {
-            return this._analysis === null ? -1 : this._analysis.url.split('/').slice(-2, -1)[0];
+            return this._analysis == null ? -1 : this._analysis.url.split('/').slice(-2, -1)[0];
         }
     }
 };
@@ -101,52 +101,48 @@ export default {
 <template>
     <tr>
         <td>
-            <div v-if="_analysis !== null && _analysis.task_state == 'su'" class="btn btn-default bg-success disabled">
+            <div v-if="_analysis != null && _analysis.task_state == 'su'" class="btn btn-default bg-success disabled">
                 <i class="fa fa-check text-white"></i>
             </div>
-            <div v-if="_analysis !== null && _analysis.task_state == 'fa'" class="btn btn-default bg-danger disabled">
+            <div v-if="_analysis != null && _analysis.task_state == 'fa'" class="btn btn-default bg-danger disabled">
                 <i class="fa fa-circle text-white"></i>
             </div>
-            <div v-if="_analysis === null || _analysis.task_state == 'pe' || _analysis.task_progress === null"
+            <div v-if="_analysis == null || _analysis.task_state == 'pe' || _analysis.task_progress == null"
                  class="btn btn-default bg-light disabled">
                 <div class="spinner text-white"></div>
             </div>
-            <div v-if="_analysis !== null && _analysis.task_state == 'st' && _analysis.task_progress !== null"
+            <div v-if="_analysis != null && _analysis.task_state == 'st' && _analysis.task_progress != null"
                  class="btn btn-default bg-light disabled">
                 {{ Math.round(_analysis.task_progress.percent) }} %
             </div>
         </td>
-        <td v-if="_analysis === null">
+        <td v-if="_analysis == null">
             <p>Fetching analysis status, please wait...</p>
         </td>
-        <td v-if="_analysis !== null">
-            <p v-if="_function === null">
+        <td v-if="_analysis != null">
+            <p v-if="_function == null || _subject == null">
                 <div class="spinner"></div>
                 Retrieving function information...
             </p>
-            <p v-if="_function !== null">
-                Function name: <i>{{ _function.name }}</i>
-            </p>
-            <p v-if="_subject === null">
-                <div class="spinner"></div>
-                Retrieving function information...
-            </p>
-            <p v-if="_subject !== null">
-                Subject name: <i>{{ _subject.name }}</i>
+            <p v-if="_function != null && _subject != null">
+                <b>Function <i>{{ _function.name }}</i> on subject <i>{{ _subject.name }}</i></b>
             </p>
             <p>
-                Parameters: <i>{{ _analysis.kwargs }}</i>
+                <b>Parameters:</b> {{ _analysis.kwargs }}
             </p>
             <p v-if="_analysis.task_state == 'su'">
-                This task was created on {{ new Date(_analysis.creation_time) }},
-                started running {{ new Date(_analysis.start_time) }}
-                and ran for {{ Math.round(_analysis.duration) }} seconds.
+                <span><b>Created on:</b> {{ new Date(_analysis.creation_time) }}
+                    &#8212; <b>Started at:</b> {{ new Date(_analysis.start_time) }}
+                    &#8212; <b>Duration:</b> {{ Math.round(_analysis.duration) }} seconds</span>
+                <span v-if="_analysis.task_memory != null">
+                    &#8212; <b>Peak memory usage:</b> {{ Math.round(_analysis.task_memory / 1024 / 1024) }} MB
+                </span>
             </p>
             <p v-if="_analysis.task_state == 'fa'">
                 This task was created on {{ new Date(_analysis.creation_time) }},
                 started running {{ new Date(_analysis.start_time) }}
-                but failed
-                <span v-if="_error !== null">
+                but failed.
+                <span v-if="_error != null">
             with message: <i>{{ _error }}</i>
           </span>
             </p>
