f078622484493ab42be8fb95789a71ab0ce0dae0
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-02-05 10:47:23 +0100

    TST: Moved orcid_socialapp to fixtures module

--- ce_ui/tests/conftest.py
+++ ce_ui/tests/conftest.py
@@ -1,11 +1,11 @@
 import pytest
 
-from allauth.socialaccount.models import SocialApp
-
 from topobank.fixtures import handle_usage_statistics, sync_analysis_functions, test_analysis_function  # noqa: F401
 from topobank.manager.tests.utils import two_topos, user_three_topographies_three_surfaces_three_tags, \
     UserFactory  # noqa: F401
 from topobank.organizations.tests.utils import OrganizationFactory
+
+from .fixtures import orcid_socialapp  # noqa: F401
 
 
 @pytest.mark.django_db
@@ -16,10 +16,3 @@ def user_with_plugin():
     user = UserFactory()
     user.groups.add(org.group)
     return user
-
-
-@pytest.mark.django_db
-@pytest.fixture
-def orcid_socialapp():
-    social_app = SocialApp.objects.create(provider='orcid', name='ORCID')
-    social_app.sites.set([1])
