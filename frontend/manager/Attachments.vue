d0370c85ec3339904b5682a6c2d539ca698a1437
Ioannis Nezis <ioannis@nezis.de>
2024-04-26 12:12:01 +0200

    ENH: delete-button for attachachments

--- frontend/manager/Attachments.vue
+++ frontend/manager/Attachments.vue
@@ -14,6 +14,8 @@ const props = defineProps({
 });
 
 
+const deleteAttachmentIdx = ref(-1);
+
 function onProgress(e) {
     console.log(e);
 }
@@ -81,6 +83,19 @@ function handleFileDrop(files) {
     }
 }
 
+function deleteAttachment(index) {
+    if (index < 0 || index >= props.attachments.length) {
+        console.error(`The provided index (${index}) is out of range.`);
+        return
+    }
+    const attachment = props.attachments[index];
+    axios.delete(attachment.url)
+        .then(() => {
+            props.attachments.splice(index, 1);
+            deleteAttachmentIdx.value = -1;
+        })
+}
+
 function formatDateTime(dateTimeString) {
     var date = new Date(dateTimeString);
     return date.toLocaleString();
@@ -88,6 +103,13 @@ function formatDateTime(dateTimeString) {
 
 const isEditable = computed(() => {
     return ['edit', 'full'].includes(props.permission);
+})
+
+const attachmentToDelete = computed(() => {
+    if (deleteAttachmentIdx.value >= 0) {
+        return props.attachments[deleteAttachmentIdx.value];
+    }
+    return null;
 })
 
 </script>
@@ -100,20 +122,56 @@ const isEditable = computed(() => {
             </div>
         </template>
         <b-card-body>
-            <div v-if="!isEditable">
-                This digital surface twin does not have file attachments yet.
-            </div>
             <drop-zone @files-dropped="handleFileDrop">
                 Drop your attachments here or
             </drop-zone>
-            <div class="border p-3 rounded mt-5">
-                <div v-for="attachment in attachments">
-                    <div class="d-flex">
-                        <a :href="attachment.file"><i class="fa-solid fa-paperclip me-3"></i>{{ attachment.name }} </a>
+            <div class=" mt-5">
+                <div v-for="(attachment, index) in attachments" :key="attachment.id">
+                    <div class="d-flex align-items-center my-1 border rounded px-2 py-1">
+                        <a :href="attachment.file"><i class="fa-solid fa-paperclip me-3"></i>{{ attachment.name }}
+                        </a>
                         <span class="ms-auto"> created: {{ formatDateTime(attachment.created) }}</span>
+                        <b-button size="sm" class="ms-3" title="delete" variant="outline-danger" data
+                            data-toggle="modal" data-target="#deleteModal" @click="deleteAttachmentIdx = index">
+                            <i class="fa-solid fa-trash"></i> Delete
+                        </b-button>
                     </div>
+                </div>
+                <div v-if="attachments.length == 0">
+                    <BAlert :model-value="true" variant="primary">
+                        This digital surface twin does not have file attachments yet.
+                    </BAlert>
                 </div>
             </div>
         </b-card-body>
     </b-card>
+
+    <div id="deleteModal" v-if="deleteAttachmentIdx != -1" class="modal fade" tabindex="-1" role="dialog"
+        aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalLabel">
+                        Delete Attachment
+                    </h5>
+                </div>
+                <div class="modal-body">
+                    <div class="d-flex flex-column align-items-center">
+                        <span class="fw-bold"> This operation will permanently delete the attachment:</span>
+                        <span class="fst-italic"> "{{ attachmentToDelete.name }}" </span>
+                        <span>The operation cannot be undone!</span>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <b-button size="xl" variant="outline-secondary" data-dismiss="modal">
+                        Cancel
+                    </b-button>
+                    <b-button size="xl" class="ms-2" variant="outline-danger" data-dismiss="modal"
+                        @click="deleteAttachment(deleteAttachmentIdx)">
+                        Delete
+                    </b-button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
