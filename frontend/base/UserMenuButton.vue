c2a0bbcd8b97451d935c9caa0ffe86b4550d3915
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-11-12 21:09:45 +0100

    MAINT: Nicer user menu

--- frontend/base/UserMenuButton.vue
+++ frontend/base/UserMenuButton.vue
@@ -1,16 +1,18 @@
 <script setup>
 
 import {ref} from "vue";
+import {BNav} from "bootstrap-vue-next";
 
 import UserMenuOffcanvas from "topobank/base/UserMenuOffcanvas.vue";
 
 const props = defineProps({
-  isAnonymous: Boolean,
-  apiUrl: String,
-  userName: String,
-  name: String,
-  orcid: String,
-  isStaff: Boolean
+    isAnonymous: Boolean,
+    apiUrl: String,
+    adminUrl: String,
+    userName: String,
+    name: String,
+    orcid: String,
+    isStaff: Boolean
 });
 
 const offcanvasVisible = ref(false);
@@ -18,16 +20,19 @@ const offcanvasVisible = ref(false);
 </script>
 
 <template>
-  <a class="nav-link me-3" id="user-dropdown" role="button"
-     @click="offcanvasVisible = !offcanvasVisible">
-    <i class="fa fa-user-circle fa-fw" aria-hidden="true"></i>
-    <span v-if="!isAnonymous" class="ms-1">{{ name }}</span>
-  </a>
-  <UserMenuOffcanvas
-      v-model:visible="offcanvasVisible"
-      :api-url="apiUrl"
-      :name="name"
-      :orcid="orcid"
-      :is-staff="isStaff"
-  ></UserMenuOffcanvas>
+    <a class="nav-link me-3" id="user-dropdown" role="button"
+       @click="offcanvasVisible = !offcanvasVisible">
+        <i class="fa fa-user-circle fa-fw" aria-hidden="true"></i>
+        <span v-if="!isAnonymous" class="ms-1">{{ name }}</span>
+    </a>
+    <BNav>
+        <UserMenuOffcanvas
+            v-model:visible="offcanvasVisible"
+            :api-url="apiUrl"
+            :admin-url="adminUrl"
+            :name="name"
+            :orcid="orcid"
+            :is-staff="isStaff"
+        ></UserMenuOffcanvas>
+    </BNav>
 </template>
