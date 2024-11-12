17cb9204a0d1d6b19b6644550695b57874eae8be
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-11-12 16:24:26 +0100

    WIP: Nicer user menu

--- frontend/base/UserMenuButton.vue
+++ frontend/base/UserMenuButton.vue
@@ -5,10 +5,12 @@ import {ref} from "vue";
 import UserMenuOffcanvas from "topobank/base/UserMenuOffcanvas.vue";
 
 const props = defineProps({
-    isAnonymous: Boolean,
-    apiUrl: String,
-    userName: String,
-    name: String
+  isAnonymous: Boolean,
+  apiUrl: String,
+  userName: String,
+  name: String,
+  orcid: String,
+  isStaff: Boolean
 });
 
 const offcanvasVisible = ref(false);
@@ -16,16 +18,16 @@ const offcanvasVisible = ref(false);
 </script>
 
 <template>
-    <a class="nav-link me-3" id="user-dropdown" role="button"
-       @click="offcanvasVisible = !offcanvasVisible">
-        <i class="fa fa-user-circle fa-fw" aria-hidden="true"></i>
-        <span v-if="!isAnonymous" class="ms-1">
-            {{ name }}
-        </span>
-    </a>
-    <UserMenuOffcanvas
-        v-model:visible="offcanvasVisible"
-        :api-url="apiUrl"
-        :name="name"
-    ></UserMenuOffcanvas>
+  <a class="nav-link me-3" id="user-dropdown" role="button"
+     @click="offcanvasVisible = !offcanvasVisible">
+    <i class="fa fa-user-circle fa-fw" aria-hidden="true"></i>
+    <span v-if="!isAnonymous" class="ms-1">{{ name }}</span>
+  </a>
+  <UserMenuOffcanvas
+      v-model:visible="offcanvasVisible"
+      :api-url="apiUrl"
+      :name="name"
+      :orcid="orcid"
+      :is-staff="isStaff"
+  ></UserMenuOffcanvas>
 </template>
--- frontend/base/UserMenuOffcanvas.vue
+++ frontend/base/UserMenuOffcanvas.vue
@@ -1,18 +1,43 @@
 <script setup>
 
-import {BOffcanvas} from "bootstrap-vue-next";
+import {BNavbar, BNavbarNav, BNavItem, BOffcanvas} from "bootstrap-vue-next";
 
 const visible = defineModel("visible");
 
 const props = defineProps({
-    name: String,
-    apiUrl: String
+  apiUrl: String,
+  name: String,
+  orcid: String,
+  isStaff: Boolean
 });
 
 </script>
 
 <template>
+  <BNavbar fixed="top">
     <BOffcanvas v-model="visible" placement="end">
-        <h3>{{ name }}</h3>
+      <h3 class="text-center">{{ name }}</h3>
+      <h5 class="text-center"><a :href="`https://orcid.org/${orcid}`">{{ orcid }}</a></h5>
+
+      <BNavbarNav class="justify-content-end flex-grow-1 pe-3 mb-3">
+        <BNavItem href="">User profile</BNavItem>
+      </BNavbarNav>
+      <BNavbarNav v-if="isStaff" class="justify-content-end flex-grow-1 pe-3 mb-3">
+        <BNavItem href="">Admin interface</BNavItem>
+        <BNavItem href="">Watchman dashboard</BNavItem>
+        <BNavItem href="">Watchman status (JSON)</BNavItem>
+      </BNavbarNav>
+      <BNavbarNav class="justify-content-end flex-grow-1 pe-3 mb-3">
+        <BNavItem>Version information</BNavItem>
+        <BNavItem href="">Terms &amp; conditions</BNavItem>
+        <BNavItem href="">Help</BNavItem>
+        <BNavItem href="https://github.com/ContactEngineering/TopoBank/discussions">Feedback</BNavItem>
+        <BNavItem>Contact</BNavItem>
+        <BNavItem href="https://doi.org/10.1088/2051-672X/ac860a">Read our paper!</BNavItem>
+      </BNavbarNav>
+      <BNavbarNav class="justify-content-end flex-grow-1 pe-3">
+        <BNavItem>Sign out</BNavItem>
+      </BNavbarNav>
     </BOffcanvas>
+  </BNavbar>
 </template>
