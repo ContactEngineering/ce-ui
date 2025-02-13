eed51b224f333d85cf82c9cebf1fe956b3da4570
Ioannis Nezis <ioannis@nezis.de>
2025-02-13 17:45:41 +0100

    MAINT: rework publish progess bar

--- frontend/publish/Publish_progess.vue
+++ frontend/publish/Publish_progess.vue
@@ -3,56 +3,43 @@ const props = defineProps({
   stage: Number,
 });
 
-function highlight(n) {
-  if (n == props.stage) {
-    return "text-primary";
+const stages = [
+  {
+    name: "Publish warning",
+    icon: 'fa-info'
+  },
+  {
+    name: "Authors",
+    icon: 'fa-users-rectangle'
+  },
+  {
+    name: "License",
+    icon: 'fa-scale-balanced'
+  },
+  {
+    name: "Sublit",
+    icon: 'fa-paper-plane'
   }
-  else if (n < props.stage) {
-    return "text-success";
-  }
-  else {
-    return "text-muted";
-  }
-}
+];
+
 </script>
 <template>
   <h3>Publish</h3>
-  <div id="progress" class="d-flex flex-row justify-content-evenly">
-    <div class="d-flex flex-column align-items-center" :class="highlight(0)">
-      <span class="fa-stack fa-lg">
-        <i class="fa-regular fa-circle fa-stack-2x"></i>
-        <i class="fa-solid fa-info fa-stack-1x"></i>
-      </span>
-      <span>
-        Publish warning
-      </span>
-    </div>
-    <div class="d-flex flex-column align-items-center" :class="highlight(1)">
-      <span class="fa-stack fa-lg">
-        <i class="fa-regular fa-circle fa-stack-2x"></i>
-        <i class="fa-solid fa-users-rectangle fa-stack-1x"></i>
-      </span>
-      <span class="text-muted">
-        Authors
-      </span>
-    </div>
-    <div class="d-flex flex-column align-items-center" :class="highlight(2)">
-      <span class="fa-stack fa-lg">
-        <i class="fa-regular fa-circle fa-stack-2x"></i>
-        <i class="fa-solid fa-scale-balanced fa-stack-1x"></i>
-      </span>
-      <span class="text-muted">
-        License
-      </span>
-    </div>
-    <div class="d-flex flex-column align-items-center" :class="highlight(3)">
-      <span class="fa-stack fa-lg">
-        <i class="fa-regular fa-circle fa-stack-2x"></i>
-        <i class="fa-regular fa-paper-plane fa-stack-1x"></i>
-      </span>
-      <span class="text-muted">
-        Submit
-      </span>
+  <div id="progress" class="d-flex flex-row justify-content-center">
+    <div v-for="stage in [0, 1, 2, 3]" class="d-flex flex-row align-items-center">
+      <div class="position-relative"
+        :class="stage < props.stage ? 'text-success' : (stage == props.stage ? 'text-primary' : 'text-muted')">
+        <span class="fa-stack fa-lg">
+          <i class="fa-regular fa-circle fa-stack-2x"></i>
+          <i class="fa-solid fa-stack-1x" :class="stages[stage].icon"></i>
+        </span>
+        <span class="position-absolute top-100 start-50 translate-middle text-nowrap pt-3">
+          {{ stages[stage].name }}
+        </span>
+      </div>
+
+      <div v-if="stage != 3" :class="props.stage > stage ? 'bg-success' : 'bg-secondary'"
+        style="width:105px; height: 10px;"> </div>
     </div>
   </div>
 </template>
