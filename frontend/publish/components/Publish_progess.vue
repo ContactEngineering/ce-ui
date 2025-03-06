d316b26dace71a786fdccbccace436af43f1b2fe
Ioannis Nezis <ioannis@nezis.de>
2025-03-06 15:45:20 +0100

    ENH: better contrasts

--- frontend/publish/components/Publish_progess.vue
+++ frontend/publish/components/Publish_progess.vue
@@ -28,7 +28,7 @@ const stages = [
   <div id="progress" class="d-flex flex-row justify-content-center">
     <div v-for="stage in [0, 1, 2, 3]" class="d-flex flex-row align-items-center">
       <div class="position-relative" style="transition-property: color; transition-delay: 1s;"
-        :class="stage < props.stage ? 'text-success' : (stage == props.stage ? 'text-primary' : 'text-muted')">
+        :style="stage < props.stage ? 'color: oklch(0.627 0.194 149.214);' : (stage == props.stage ? 'color: oklch(0.546 0.245 262.881);' : 'color: oklch(0.872 0.01 258.338);')">
         <span class="fa-stack fa-lg">
           <i class="fa-regular fa-circle fa-stack-2x"></i>
           <i class="fa-solid fa-stack-1x" :class="stages[stage].icon"></i>
@@ -38,11 +38,11 @@ const stages = [
         </span>
       </div>
 
-      <div v-if="stage != 3" class="bg-success" :style="{ width: stage < props.stage ? '120px' : '0px' }"
-        style="height: 10px; transition: width 1s ease-in-out;">
+      <div v-if="stage != 3" :style="{ width: stage < props.stage ? '120px' : '0px' }"
+        style="height: 10px; transition: width 1s ease-in-out; background-color: oklch(0.627 0.194 149.214);">
       </div>
-      <div v-if="stage != 3" class="bg-secondary" :style="{ width: stage < props.stage ? '0' : '120px' }"
-        style="height: 10px; transition: width 1s ease-in-out;">
+      <div v-if="stage != 3" :style="{ width: stage < props.stage ? '0' : '120px' }"
+        style="height: 10px; transition: width 1s ease-in-out; background-color: oklch(0.872 0.01 258.338);">
       </div>
     </div>
   </div>
