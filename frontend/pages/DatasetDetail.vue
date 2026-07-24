4766b33141a55626b96a97b26f04745d04dfab5d
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2026-07-24 21:56:29 +0200

    MAINT: Updated card style for surface list

--- frontend/pages/DatasetDetail.vue
+++ frontend/pages/DatasetDetail.vue
@@ -315,11 +315,12 @@ const measurementCount = computed(() => {
                         </Toolbar>
                         <DropZone v-if="isEditable" @files-dropped="filesDropped">
                         </DropZone>
-                        <!-- Left accent bar (primary) visually separates each
-                             measurement. -->
+                        <!-- Left accent bar (primary) plus alternating row
+                             background visually separate each measurement. -->
                         <div v-for="(topography, index) in _topographies"
                              :key="index"
-                             class="border-start border-primary border-4 ps-3 mb-2">
+                             class="border-start border-primary border-4 ps-3 mb-2"
+                             :class="index % 2 === 0 ? 'bg-body' : 'bg-body-tertiary'">
                             <TopographyCard v-if="topography != null"
                                             v-model:selected="_selected[index]"
                                             v-model:topography="_topographies[index]"
